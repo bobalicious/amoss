4f2fc71b7d6c04c5a11946f737ffdd86629c40d2
Rob Baillie <bobalicious.bob@gmail.com>
2020-12-21 20:56:55 +0000

    Packager script will now create a clean package.

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,9 +2,9 @@
 git stash push --message tmp-packager
 git reset --hard
 
-rm -rf ../force-app/amoss_examples
+rm -rf ./force-app/amoss_examples
 
-sfdx force:package:version:create -p amoss -d ../force-app --wait 10 -v amoss-dev-hub -x
+sfdx force:package:version:create -p amoss -d force-app --wait 10 -v amoss-dev-hub -x
 
 git reset --hard
 git merge --squash stash
